586dae901e3359c5c30cadfcf2493bcb0c5b35cb
Cynthia Kitili <nyambucindy0@gmail.com>
2020-11-28 17:23:07 +0300

    add news.html

--- app/views.py
+++ app/views.py
@@ -8,4 +8,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message = 'All News Articles Do Not Be Left Behind'
+    return render_template('index.html', message = message)
+
+@app.route('/news/<int:news_id>')  
+def news(news_id):
+
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    return render_template('news.html',id = news_id)
+
