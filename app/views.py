f79e354ee927b87a63b81d93e6e3b61dc0e81b8c
Cynthia Kitili <nyambucindy0@gmail.com>
2020-11-28 19:05:21 +0300

    update init

--- app/views.py
+++ app/views.py
@@ -9,7 +9,8 @@ def index():
     View root page function that returns the index page and its data
     '''
     message = 'All News Articles Do Not Be Left Behind'
-    return render_template('index.html', message = message)
+    title= 'Home - Welcome to the best News Articles Site'
+    return render_template('index.html', message = message, title=title)
 
 @app.route('/news/<int:news_id>')  
 def news(news_id):
